39755cf36690e9a8531d879da9c5fbeefced7dbb
vitush <vit.ush@mail.ru>
2022-11-07 18:20:50 +0300

    add task model

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :name, :description] do |n|
     "string#{n}"
   end
 
@@ -8,4 +8,13 @@ FactoryBot.define do
   end
 
   sequence :type, ['Developer', 'Admin', 'Manager'].cycle
+
+  sequence :state, [
+    'new_task', 'in_development', 'in_qa', 'in_code_review',
+    'ready_for_release', 'released', 'archived'
+  ].cycle
+
+  sequence :expired_at do |n|
+    Date.current.advance(days: n).to_s
+  end
 end
