3046898ed9aaa8bedbd105eb880707fbd7cfa10f
vitush <vit.ush@mail.ru>
2022-11-21 16:51:22 +0300

    add task read route

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -5,4 +5,11 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     get :index, params: { format: :json }
     assert_response :success
   end
+
+  test 'should get show' do
+    author = create(:user)
+    task = create(:task, author: author)
+    get :show, params: { id: task.id, format: :json }
+    assert_response :success
+  end
 end
