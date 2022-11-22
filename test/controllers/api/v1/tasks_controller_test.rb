9a6a59d75298358431c6750fba3caf0afe99b200
vitush <vit.ush@mail.ru>
2022-11-22 11:34:44 +0300

    add task creation route

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -12,4 +12,20 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     get :show, params: { id: task.id, format: :json }
     assert_response :success
   end
+
+  test 'should post create' do
+    author = create(:user)
+    sign_in(author)
+    assignee = create(:user)
+    task_attributes = attributes_for(:task).merge({ assignee_id: assignee.id })
+    post :create, params: { task: task_attributes, format: :json }
+    assert_response :created
+
+    data = JSON.parse(response.body)
+    created_task = Task.find(data['task']['id'])
+
+    assert created_task.present?
+    assert_equal task_attributes.merge({ state: 'new_task' }).stringify_keys,
+                 created_task.slice(*task_attributes.keys)
+  end
 end
