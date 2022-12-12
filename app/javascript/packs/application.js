d6c4149f4498261580d5ecb85d4c2b80f0606c2d
vitush <vit.ush@mail.ru>
2022-12-12 18:44:50 +0300

    remove unnecessary code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'material-design-lite/material.js';
 
-import 'channels';
-
 import WebpackerReact from 'webpacker-react';
 import TaskBoard from '../components/TaskBoard';
 
