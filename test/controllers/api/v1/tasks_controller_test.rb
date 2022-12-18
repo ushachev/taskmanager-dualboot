73ec2948a12216274d61e9973f8fd431f8f04fae
vitush <vit.ush@mail.ru>
2022-12-18 15:34:20 +0300

    add user notification about actions with task

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -18,7 +18,10 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     sign_in(author)
     assignee = create(:user)
     task_attributes = attributes_for(:task).merge({ assignee_id: assignee.id })
-    post :create, params: { task: task_attributes, format: :json }
+
+    assert_emails 1 do
+      post :create, params: { task: task_attributes, format: :json }
+    end
     assert_response :created
 
     data = JSON.parse(response.body)
@@ -38,7 +41,9 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
       except(:state).
       stringify_keys
 
-    patch :update, params: { id: task.id, format: :json, task: task_attributes }
+    assert_emails 1 do
+      patch :update, params: { id: task.id, format: :json, task: task_attributes }
+    end
     assert_response :success
 
     task.reload
@@ -48,7 +53,10 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
   test 'should delete destroy' do
     author = create(:user)
     task = create(:task, author: author)
-    delete :destroy, params: { id: task.id, format: :json }
+
+    assert_emails 1 do
+      delete :destroy, params: { id: task.id, format: :json }
+    end
     assert_response :success
 
     assert !Task.where(id: task.id).exists?
