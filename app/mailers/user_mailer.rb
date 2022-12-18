a1c6a1dbb592d799e425fa57c6f311a5a6cd62a2
vitush <vit.ush@mail.ru>
2022-12-18 15:34:20 +0300

    add user notification about actions with task

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,22 @@
 class UserMailer < ApplicationMailer
   default from: 'noreply@taskmanager.com'
+
+  def task_created
+    user = params[:user]
+    @task = params[:task]
+
+    mail(to: user.email, subject: 'New Task Created')
+  end
+
+  def task_updated
+    @task = params[:task]
+
+    mail(to: @task.author.email, subject: 'Task Updated')
+  end
+
+  def task_deleted
+    @task = params[:task]
+
+    mail(to: @task.author.email, subject: 'Task Deleted')
+  end
 end
