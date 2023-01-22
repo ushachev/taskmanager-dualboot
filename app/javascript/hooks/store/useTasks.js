0a5b7a5eeea917381d876f5203d1c173752799f7
vitush <vit.ush@mail.ru>
2023-01-22 13:48:38 +0300

    add app store

--- app/javascript/hooks/store/useTasks.js
+++ app/javascript/hooks/store/useTasks.js
@@ -1,111 +1,44 @@
-import { useState, useEffect } from 'react';
-import { propOr } from 'ramda';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import TasksRepository from 'repositories/TasksRepository.js';
+import { STATES } from 'presenters/TaskPresenter';
+import * as TaskActions from 'slices/TasksSlice';
 import TaskForm from 'forms/TaskForm';
 
-const STATES = [
-  { key: 'new_task', value: 'New' },
-  { key: 'in_development', value: 'In Dev' },
-  { key: 'in_qa', value: 'In QA' },
-  { key: 'in_code_review', value: 'in CR' },
-  { key: 'ready_for_release', value: 'Ready for release' },
-  { key: 'released', value: 'Released' },
-  { key: 'archived', value: 'Archived' },
-];
-
 const useTasks = () => {
-  const [board, setBoard] = useState({
-    columns: STATES.map((column) => ({
-      id: column.key,
-      title: column.value,
-      cards: [],
-      meta: {},
-    })),
-  });
-  const [boardCards, setBoardCards] = useState({});
-
-  const getTasksByState = (state, page = 1, perPage = 10) =>
-    TasksRepository.index({
-      q: { stateEq: state, s: 'id DESC' },
-      page,
-      perPage,
-    });
-
-  const setBoardCardsForState = (state, { items: cards, meta }) => {
-    setBoardCards((prevCards) => ({ ...prevCards, [state]: { cards, meta } }));
-  };
+  const board = useSelector((state) => state.TasksSlice.board);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    STATES.forEach(async ({ key: state }) => {
-      const cardsData = await getTasksByState(state);
-      setBoardCardsForState(state, cardsData);
-    });
+    STATES.forEach(({ key: state }) => dispatch(TaskActions.selectTasks({ state })));
   }, []);
-  useEffect(() => {
-    const columns = STATES.map(({ key, value }) => ({
-      id: key,
-      title: value,
-      cards: propOr([], 'cards', boardCards[key]),
-      meta: propOr({}, 'meta', boardCards[key]),
-    }));
 
-    setBoard({ columns });
-  }, [boardCards]);
+  const appendCards = (state, page) => dispatch(TaskActions.selectMoreTasks({ state, page }));
 
-  const appendCards = async (state, page) => {
-    const { items: cards, meta } = await getTasksByState(state, page);
-
-    setBoardCards((prevCards) => ({
-      ...prevCards,
-      [state]: { cards: prevCards[state].cards.concat(cards), meta },
-    }));
-  };
-
-  const moveCard = async (task, { toColumnId }) => {
+  const moveCard = (task, { toColumnId }) => {
     const transition = task.transitions.find(({ to }) => toColumnId === to);
 
     if (transition) {
-      await TasksRepository.update(task.id, { stateEvent: transition.event });
-
-      const destinationCardsData = await getTasksByState(toColumnId);
-      const sourceCardsData = await getTasksByState(task.state);
-
-      setBoardCardsForState(toColumnId, destinationCardsData);
-      setBoardCardsForState(task.state, sourceCardsData);
+      dispatch(TaskActions.changeTaskState({ task, stateEvent: transition.event }));
     }
   };
 
-  const createTask = async (params) => {
+  const createTask = (params) => {
     const attributes = TaskForm.attributesToSubmit(params);
-    const task = await TasksRepository.create(attributes);
-    const cardsData = await getTasksByState(task.state);
-
-    setBoardCardsForState(task.state, cardsData);
+    dispatch(TaskActions.createTask(attributes));
   };
 
-  const updateTask = async (task) => {
+  const updateTask = (task) => {
     const attributes = TaskForm.attributesToSubmit(task);
-
-    await TasksRepository.update(task.id, attributes);
-
-    const cardsData = await getTasksByState(task.state);
-
-    setBoardCardsForState(task.state, cardsData);
+    dispatch(TaskActions.updateTask({ task, attributes }));
   };
 
-  const destroyTask = async (task) => {
-    await TasksRepository.destroy(task.id);
-
-    const cardsData = await getTasksByState(task.state);
-
-    setBoardCardsForState(task.state, cardsData);
-  };
+  const destroyTask = (task) => dispatch(TaskActions.destroyTask(task));
 
   return {
     board,
     appendCards,
-    getCard: TasksRepository.show,
+    getCard: TaskActions.selectTask,
     moveCard,
     createTask,
     updateTask,
---
+++ app/javascript/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import TasksSlice from 'slices/TasksSlice';
+
+const store = configureStore({
+  reducer: {
+    TasksSlice,
+  },
+});
+
+export default store;
