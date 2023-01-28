d292bc8438885eb7b7fc7478d18efbe61ab5c921
vitush <vit.ush@mail.ru>
2023-01-28 18:57:05 +0300

    fix

--- app/services/attachments_service.rb
+++ app/services/attachments_service.rb
@@ -1,7 +1,7 @@
 module AttachmentsService
   class << self
     def file_url(file)
-      Rails.application.routes.url_helpers.rails_blob_url(file)
+      Rails.application.routes.url_helpers.rails_blob_url(file, only_path: true)
     end
   end
 end
