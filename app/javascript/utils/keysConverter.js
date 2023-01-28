0108d72281033f12444daa53665c5b6039827ed7
vitush <vit.ush@mail.ru>
2023-01-28 16:50:48 +0300

    improve decamelize converter without Blob corruption

--- app/javascript/utils/keysConverter.js
+++ app/javascript/utils/keysConverter.js
@@ -1,4 +1,15 @@
 import humps from 'humps';
+import { is } from 'ramda';
 
-export const decamelize = (obj) => humps.decamelizeKeys(obj);
+export const decamelize = (obj) => {
+  if (!is(Object, obj) || is(Blob, obj) || is(Function, obj) || is(Date, obj)) return obj;
+
+  if (is(Array, obj)) return obj.map(decamelize);
+
+  return Object.entries(obj).reduce(
+    (acc, [key, value]) => ({ ...acc, [humps.decamelize(key)]: decamelize(value) }),
+    {},
+  );
+};
+
 export const camelize = (obj) => humps.camelizeKeys(obj);
